5b3b3826e311ddf477285621b1b68d7b0e6cbcb7
JoseAngelFlorentino <1114056@est.intec.edu.do>
2024-07-10 15:56:58 -0400

    Add project files.

---
+++ AppLogger/Class1.cs
@@ -0,0 +1,42 @@
+﻿namespace AppLogger
+{
+    public class Dependencies
+    {
+        private readonly Dictionary<string, List<string>> _directDependencies = new Dictionary<string, List<string>>();
+
+        public void AddDirect(string item, List<string> dependencies)
+        {
+            if (!_directDependencies.ContainsKey(item))
+            {
+                _directDependencies[item] = new List<string>();
+            }
+            _directDependencies[item].AddRange(dependencies);
+        }
+
+        public List<string> DependenciesFor(string item)
+        {
+            var result = new HashSet<string>();
+            var visited = new HashSet<string>();
+            CalculateDependencies(item, result, visited);
+            return result.ToList();
+        }
+
+        private void CalculateDependencies(string item, HashSet<string> result, HashSet<string> visited)
+        {
+            if (!visited.Contains(item))
+            {
+                visited.Add(item);
+                if (_directDependencies.ContainsKey(item))
+                {
+                    foreach (var dependency in _directDependencies[item])
+                    {
+                        if (result.Add(dependency))
+                        {
+                            CalculateDependencies(dependency, result, visited);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
